e0f90133c854290528e5b8bbc86a2c644c5e952a
Giovanni Bassi <giggio@giggio.net>
2013-03-20 09:05:30 -0300

    first

---
+++ ChatClient/ChatClient/js/socketconnect.coffee
@@ -0,0 +1,10 @@
+﻿$ ->
+  socket = io.connect("http://testeazuresummit.azurewebsites.net/")
+  socket.on 'news', (data) -> show(data)
+  $('#btn').on 'click', ->
+    inputmsg = $('#msg')
+    msg = inputmsg.val()
+    inputmsg.val ''
+    socket.emit 'message', msg
+show = (msg) ->
+  $('#messages').append "<li>#{msg}</li>"
---
+++ ChatClient/ChatClient/js/socketconnect.js
@@ -0,0 +1,23 @@
+﻿(function() {
+  var show;
+
+  $(function() {
+    var socket;
+    socket = io.connect("http://testeazuresummit.azurewebsites.net/");
+    socket.on('news', function(data) {
+      return show(data);
+    });
+    return $('#btn').on('click', function() {
+      var inputmsg, msg;
+      inputmsg = $('#msg');
+      msg = inputmsg.val();
+      inputmsg.val('');
+      return socket.emit('message', msg);
+    });
+  });
+
+  show = function(msg) {
+    return $('#messages').append("<li>" + msg + "</li>");
+  };
+
+}).call(this);
